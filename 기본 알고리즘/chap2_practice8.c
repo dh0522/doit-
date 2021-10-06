71b2a7898aa54a7bebda1fe2b3e509f84036c905
gong4857 <gong4857@naver.com>
2021-10-06 19:50:02 +0900

    211006

---
+++ 기본 알고리즘/chap2_practice8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+void ary_copy(int a[], const int b[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		a[i] = b[i];
+	}
+
+}
+int main() {
+	int n;
+	int* a, * b;
+
+	printf("요소 개수는? ");
+	scanf("%d", &n);
+	a = calloc(n, sizeof(int));
+	b = calloc(n, sizeof(int));
+
+	int i;
+	printf("%d개의 정수를 입력하세요.\n", n);
+	for (i = 0; i < n; i++)
+	{
+		printf("b[%d]: ", i);
+		scanf("%d", &b[i]);
+	}
+	ary_copy(a, b, n);
+	printf("배열 b의 원소를 배열a에 모두 복사했습니다.\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("a[%d]=%d\n", i, a[i]);
+	}
+
+	free(b);
+	free(a);
+}
